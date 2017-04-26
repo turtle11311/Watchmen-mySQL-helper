73f64c9c32e82304948ee0288eee4b56e707fd89
Wong, Fu-Lian <turtle11311@gmail.com>
2017-04-27 01:11:27 +0800

    Add configfile

---
+++ config/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  mysql_config: {
+    connectionLimit: 10,
+    host: 'IP',
+    user: 'USER',
+    password: 'MYSQL_PASSWD',
+    database: 'DB_NAME'
+  },
+  port: 80
+}
