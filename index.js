45c40c8bdf047da44d6d8314c40015a7ce66afb2
Wong, Fu-Lian <turtle11311@gmail.com>
2017-04-27 03:09:12 +0800

    Add POST for /heartrate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let express = require('express')
-let MySQL = require('mysql')
+let MySQL = require('promise-mysql')
 let bodyParser = require('body-parser')
 let http = require('http')
 
@@ -12,6 +12,14 @@ let config = require('./config')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 let mysql = MySQL.createPool(config.mysql_config)
+
+app.post('/heartrate', (req, res) => {
+  mysql.query(`SELECT UNIX_TIMESTAMP(CONCAT(DateTime, ' ', StartTime)) \
+    - UNIX_TIMESTAMP(CONCAT(DateTime, ' ', '00:00:00')) AS sec, ROUND(AVG(Value)) AS val \
+    FROM Heartrate_Detail GROUP BY sec ORDER BY sec`)
+    .then(rows => res.send(rows))
+    .catch(err => res.status(500))
+})
 
 let server = http.createServer(app)
 server.listen(config.port)
