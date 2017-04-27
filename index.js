90c41cda0bf90a3f71d6707e3941abfeceefa776
Wong, Fu-Lian <turtle11311@gmail.com>
2017-04-27 16:15:45 +0800

    GET /heartrate/detail

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let MySQL = require('promise-mysql')
 let bodyParser = require('body-parser')
 let http = require('http')
 
-let debug = require('debug')('Watchmen')
 let app = express()
 let config = require('./config')
 
@@ -14,11 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 let mysql = MySQL.createPool(config.mysql_config)
 
-app.post('/heartrate', (req, res) => {
+app.get('/heartrate/detail', (req, res) => {
   let queryStr = `SELECT UNIX_TIMESTAMP(\
   CONCAT(DateTime, ' ', StartTime)) - UNIX_TIMESTAMP(CONCAT(DateTime, ' ', '00:00:00')\
   ) AS sec, Value AS val FROM Heartrate_Detail \
-    WHERE User_ID='${req.body.user}' AND DateTime='${req.body.date}' ORDER BY sec`
+    WHERE User_ID='${req.query.user}' AND DateTime='${req.query.date}' ORDER BY sec`
   mysql.query(queryStr)
     .then(rows => res.send(rows))
     .catch(err => res.send(err))
